f1d57b8c303517035ecb4b2184f64435a0ee0c41
muhammadsalman345 <salmankhanhissam345@gmail.com>
2024-02-10 04:36:48 -0800

    otp componet 100% working header footer

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,7 +9,7 @@ import { users } from '../interfaces/auth';
 })
 export class AuthService {
   // private baseUrl = 'http://localhost:3000/api';
-  private baseUrl = 'https://adorable-waders-crab.cyclic.app';
+  private baseUrl = 'https://plain-suit-moth.cyclic.app/';
 
   constructor(private http: HttpClient) {}
 
